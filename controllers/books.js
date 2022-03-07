dca28ac5ab8b70e12638e46581eac7c97eb1db6f
Ajmal Hakim <samimajmal@yahoo.com>
2022-03-07 08:20:05 -0500

    add book controller

--- controllers/books.js
+++ controllers/books.js
@@ -3,21 +3,35 @@ import { Book } from "../models/book.js";
 
 
 function index(req, res) {
-  Book.find({})
+   Book.find({})
   .then(books => {
     res.render('books/index', {
       books,
       title: "Book",
-    })
-  })
+       })
+   })
   .catch(err => {
     console.log(err)
     res.redirect("/books")
   })
-  console.log(user);
+
+}
+
+function create(req, res) {
+  req.body.owner = req.user.profile._id
+	req.body.read = !!req.body.read
+  Book.create(req.body)
+  .then(book => {
+    res.redirect('/books')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/books')
+  })
 }
 
 export {
 
-  index
+  index,
+  create
 }
--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import * as booksCtrl from '../controllers/books.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router() 
 
 router.get('/',booksCtrl.index)
+// POST - localhost:3000/profiles/new
+router.post('/',isLoggedIn,booksCtrl.create)
 
 export {
   router
