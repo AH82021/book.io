9a84c318d4b29738ddb1975a9234c00807fd51e9
Ajmal Hakim <samimajmal@yahoo.com>
2022-03-07 16:26:35 -0500

    add review model

--- models/book.js
+++ models/book.js
@@ -1,17 +1,30 @@
 import mongoose from "mongoose";
 
+
+
+
 const Schema =mongoose.Schema
+
+const reviewSchema = new Schema({
+  content: String,
+  rating: {type: Number, min: 1, max: 5, default: 5}
+}, {
+  timestamps: true
+})
 const bookSchema = new Schema({
 
   title :String,
   author:String,
   genre :String,
   read :Boolean,
-  owner: {type: Schema.Types.ObjectId, ref: "Profile"}
-
-
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"},
+  reviews :[reviewSchema],
+  
+  }, { timestamps: true 
 
 }) 
+
+
 const Book = mongoose.model('Book', bookSchema)
 
 
